646506da0e302e0118cb3d10336486b44ff78c50
imujo <ivo.mujo.3@gmail.com>
2024-10-15 16:55:36 +0200

    spring initializr setup

---
+++ src/main/java/com/progi/ProgiApplication.java
@@ -0,0 +1,13 @@
+package com.progi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgiApplication.class, args);
+	}
+
+}
