456e24c101fdc9b5b46ea37e910a9726edf1a740
imujo-fer <ivo.mujo@fer.unizg.hr>
2024-10-18 07:58:45 +0200

    add sample test

--- src/test/java/com/progi/ProgiApplicationTests.java
+++ src/test/java/com/progi/ProgiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.progi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ProgiApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void testGreet() {
+        String expectedGreeting = "Hello, World!";
+        String actualGreeting = "Hello, World!";
+        Assertions.assertEquals(expectedGreeting, actualGreeting, "The greeting message should match the expected output");
+    }
 
 }
