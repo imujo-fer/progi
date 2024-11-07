71c834c0ed5a0577189e096a7b33cdb9e2ea5f4e
adrianofaletar <adriano.faletar@fer.unizg.hr>
2024-11-07 10:48:25 +0100

    - Change from patch to put

--- src/main/java/com/progi/trip/TripController.java
+++ src/main/java/com/progi/trip/TripController.java
@@ -7,15 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.progi.Enum.Status;
 import com.progi.auth.UserSessionService;
@@ -76,7 +68,7 @@ public class TripController {
         return ResponseEntity.ok(createdTrip);
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Trip> updateTrip(@PathVariable Integer id, @Valid @RequestBody TripDTO tripDTO) {
        return  ResponseEntity.ok(tripService.updateTrip(id, tripDTO));
     }
