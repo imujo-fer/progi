6f50a44764bf410544d637e2ae7fafc514e19cbb
polina-fer <polina.rykova@fer.unizg.hr>
2024-11-10 13:50:03 +0100

    connect sidebar navigation to pages

--- frontend/src/components/Layout/Sidebar.component.tsx
+++ frontend/src/components/Layout/Sidebar.component.tsx
@@ -1,34 +1,84 @@
 import { Menu } from "antd";
 import useGetCurrentUserInfo from "../../hooks/currentUser";
 import { UserDetailsDTORolesEnum } from "../../api_gen/models/user-details-dto";
+import { Link } from "@tanstack/react-router";
+import {
+  notificationsRoute,
+  pastTripsRoute,
+  tripRequestsRoute,
+} from "../../features/employee/routes/employee.routes";
+import {
+  departmentApprovalRequestsRoute,
+  departmentStatisticsRoute,
+} from "../../features/departmentHead/routes/departmentHead.routes";
+import {
+  awaitingPaymentRoute,
+  expenseReviewRequestsRoute,
+} from "../../features/accountant/routes/accountant.routes";
+import {
+  reviewTripsRoute,
+  statisticsRoute,
+} from "../../features/director/routes/director.routes";
 
 export default function Sidebar() {
   const { data } = useGetCurrentUserInfo();
   const roleList = data?.roles || [];
 
   const roles = [
-    { key: "1", label: "Trip request" },
-    { key: "2", label: "Past Trips" },
+    {
+      key: "1",
+      label: <Link to={tripRequestsRoute.to}> Trips requests </Link>,
+    },
+    { key: "2", label: <Link to={pastTripsRoute.to}> Past Trips </Link> },
   ];
 
   console.log(data);
 
   if (roleList.includes(UserDetailsDTORolesEnum.DepartmentHead)) {
-    roles.push({ key: "4", label: "Department approval requests" });
-    roles.push({ key: "5", label: "Department statistics" });
+    roles.push({
+      key: "4",
+      label: (
+        <Link to={departmentApprovalRequestsRoute.to}>
+          Department approval requests
+        </Link>
+      ),
+    });
+    roles.push({
+      key: "5",
+      label: (
+        <Link to={departmentStatisticsRoute.to}>Department statistics</Link>
+      ),
+    });
   }
 
   if (roleList.includes(UserDetailsDTORolesEnum.Accountant)) {
-    roles.push({ key: "6", label: "Expense review requests" });
-    roles.push({ key: "7", label: "Awaiting payment" });
+    roles.push({
+      key: "6",
+      label: (
+        <Link to={expenseReviewRequestsRoute.to}>Expense review requests</Link>
+      ),
+    });
+    roles.push({
+      key: "7",
+      label: <Link to={awaitingPaymentRoute.to}>Awaiting payment</Link>,
+    });
   }
 
   if (roleList.includes(UserDetailsDTORolesEnum.Director)) {
-    roles.push({ key: "8", label: "Review trips" });
-    roles.push({ key: "9", label: "Statistics" });
+    roles.push({
+      key: "8",
+      label: <Link to={reviewTripsRoute.to}>Review trips</Link>,
+    });
+    roles.push({
+      key: "9",
+      label: <Link to={statisticsRoute.to}>Statistics</Link>,
+    });
   }
 
-  roles.push({ key: "3", label: "Notifications" });
+  roles.push({
+    key: "3",
+    label: <Link to={notificationsRoute.to}>Notifications</Link>,
+  });
 
   return <Menu theme="light" items={roles} />;
 }
