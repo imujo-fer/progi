6130bea6e8dc63b98cbb8cc4ca4a6ed326adb279
polina-fer <polina.rykova@fer.unizg.hr>
2024-11-11 10:10:28 +0100

    remove unnecessary variables

--- frontend/src/pages/Register/Register.page.tsx
+++ frontend/src/pages/Register/Register.page.tsx
@@ -3,9 +3,11 @@ import RegisterForm from "./components/RegisterForm";
 
 export default function Register() {
   const { data } = useGetUserRegisterInfo({ registrationHash: "hash" });
-  const email = data?.email || "";
-  const role = data?.roles || [];
-  console.log(data);
-  const department = data?.department.name || "";
-  return <RegisterForm email={email} roles={role} department={department} />;
+  return (
+    <RegisterForm
+      email={data?.email || ""}
+      roles={data?.roles || []}
+      department={data?.department.name || ""}
+    />
+  );
 }
