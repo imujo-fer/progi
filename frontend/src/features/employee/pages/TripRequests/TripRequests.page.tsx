9b147995452211de6dee2bc3b35e91db9aa5cf46
polina-fer <polina.rykova@fer.unizg.hr>
2024-12-16 08:13:54 +0100

    add select status dropdown

--- frontend/src/features/employee/pages/TripRequests/TripRequests.page.tsx
+++ frontend/src/features/employee/pages/TripRequests/TripRequests.page.tsx
@@ -1,34 +1,5 @@
-import { Link } from "@tanstack/react-router";
-import {
-  tripRequestsCreateRoute,
-  tripRequestsEditRoute,
-} from "../../routes/employee.routes";
-import useGetTripRequests from "./hooks/useGetTripRequests";
-import { Button } from "antd";
+import TripsTable from "../../components/TripsTable.component";
 
 export default function TripRequests() {
-  const { data } = useGetTripRequests();
-  return (
-    <div>
-      <Link to={tripRequestsCreateRoute.to}>
-        <Button>Create trip request</Button>
-      </Link>
-      <br />
-      {data?.map((trip) => (
-        <div className="flex gap-4" key={trip.id}>
-          <span>{trip.requestNumber}</span>
-          <span>{trip.address}</span>
-          <span>{trip.dateFrom}</span>
-          <span>{trip.dateTo}</span>
-
-          <Link
-            to={tripRequestsEditRoute.to}
-            params={{ tripId: trip.id.toString() }}
-          >
-            <Button>Edit</Button>
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+  return <TripsTable />;
 }
