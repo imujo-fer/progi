2e9e1e52dbb71759076cccc496e0440819a5ee70
polina-fer <polina.rykova@fer.unizg.hr>
2024-12-17 16:14:55 +0100

    add create/edit expense item modal outline

--- frontend/src/features/employee/pages/SubmitExpenseReport/components/ExpenseReport.component.tsx
+++ frontend/src/features/employee/pages/SubmitExpenseReport/components/ExpenseReport.component.tsx
@@ -2,9 +2,13 @@ import Title from "antd/es/typography/Title";
 import useGetExpenseReportItems from "../hooks/useGetExpenseReportItems";
 import ExpenseReportItem from "./ExpenseReportItem";
 import { Button, Flex, List } from "antd";
+import CreateEditExpenseReportItemModal from "./CreateEditExpenseReportItemModal";
+import { useState } from "react";
 
 export default function ExpenseReport() {
   const { data } = useGetExpenseReportItems();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   if (!data || !Array.isArray(data)) {
     return <></>;
@@ -14,7 +18,7 @@ export default function ExpenseReport() {
     <div className="w-1/2 min-w-96">
       <Flex justify="space-between" align="center">
         <Title>Submit expense report</Title>
-        <Button>Add</Button>
+        <Button onClick={() => setIsOpen(true)}>Add</Button>
       </Flex>
 
       <List
@@ -25,6 +29,7 @@ export default function ExpenseReport() {
           </List.Item>
         )}
       />
+      <CreateEditExpenseReportItemModal open={isOpen} setOpen={setIsOpen} />
     </div>
   );
 }
