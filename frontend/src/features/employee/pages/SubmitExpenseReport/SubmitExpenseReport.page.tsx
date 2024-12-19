1eaea8d245d9c1ad75e4efe08780b45df4e0532a
polina-fer <polina.rykova@fer.unizg.hr>
2024-12-19 16:57:00 +0100

    separate approval requirements into a new component

--- frontend/src/features/employee/pages/SubmitExpenseReport/SubmitExpenseReport.page.tsx
+++ frontend/src/features/employee/pages/SubmitExpenseReport/SubmitExpenseReport.page.tsx
@@ -1,20 +1,13 @@
-import { Button, Card } from "antd";
+import { Button } from "antd";
 import ExpenseReport from "./components/ExpenseReport.component";
+import ApprovalRequirements from "./components/ApprovalRequirementsCard";
 
 export default function SubmitExpenseReport() {
-  const comment =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
   return (
     <div className="flex flex-col justify-between h-full p-10 py-20">
       <div className="flex lg:flex-row flex-col">
         <ExpenseReport />
-        <Card
-          title="Approval Requirements"
-          bordered={false}
-          className="lg:mx-auto max-h-fit max-w-md"
-        >
-          <p className="break-words">{comment}</p>
-        </Card>
+        <ApprovalRequirements />
       </div>
       <Button size="large">Submit for review</Button>
     </div>
---
+++ frontend/src/features/employee/pages/SubmitExpenseReport/components/ApprovalRequirementsCard.tsx
@@ -0,0 +1,13 @@
+import { Card } from "antd";
+
+export default function ApprovalRequirements() {
+  return (
+    <Card
+      title="Approval Requirements"
+      bordered={false}
+      className="lg:mx-auto max-h-fit max-w-md"
+    >
+      <p className="break-words">comment</p>
+    </Card>
+  );
+}
