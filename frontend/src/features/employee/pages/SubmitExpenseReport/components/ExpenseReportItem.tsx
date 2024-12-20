16a8cb2c0fe69db7ba72ee7d3b546d17845a777a
polina-fer <polina.rykova@fer.unizg.hr>
2024-12-20 11:34:22 +0100

    fix modal

--- frontend/src/features/employee/pages/SubmitExpenseReport/components/ExpenseReportItem.tsx
+++ frontend/src/features/employee/pages/SubmitExpenseReport/components/ExpenseReportItem.tsx
@@ -2,42 +2,44 @@ import { ExpenseReportItemWithSubcategoryDTO } from "@/api_gen";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import { Link } from "@tanstack/react-router";
 import { Card, Flex } from "antd";
+import CreateEditExpenseReportItemModal from "./CreateEditExpenseReportItemModal";
+import { useState } from "react";
 
 interface ExpenseReportItemProps {
   item: ExpenseReportItemWithSubcategoryDTO;
-  onEdit: (item: ExpenseReportItemWithSubcategoryDTO) => void;
 }
 
-export default function ExpenseReportItem({
-  item,
-  onEdit,
-}: ExpenseReportItemProps) {
+export default function ExpenseReportItem({ item }: ExpenseReportItemProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const actions = [
-    <EditOutlined
-      key="edit"
-      onClick={() => {
-        onEdit(item);
-      }}
-    />,
+    <EditOutlined key="edit" onClick={() => setIsOpen(true)} />,
     <DeleteOutlined key="delete" />,
     <Link>View bill</Link>,
   ];
+  console.log(item);
   return (
-    <Card actions={actions} className="my-3 w-full">
-      <Card.Meta
-        description={
-          <Flex justify="space-between" align="center" className="text-black">
-            <div className="w-2/3 break-words">{item.description}</div>
-            <div className="flex flex-col w-1/4">
-              <div className="ml-auto">
-                {`${item.currencyValue} ${item.currency} =   `}
-                <span className="font-semibold text-lg">{`${item.eurValue} EUR`}</span>
+    <>
+      <Card actions={actions} className="my-3 w-full">
+        <Card.Meta
+          description={
+            <Flex justify="space-between" align="center" className="text-black">
+              <div className="w-2/3 break-words">{item.description}</div>
+              <div className="flex flex-col w-1/4">
+                <div className="ml-auto">
+                  {`${item.currencyValue} ${item.currency} =   `}
+                  <span className="font-semibold text-lg">{`${item.eurValue} EUR`}</span>
+                </div>
+                <div className="ml-auto">{item.expenseSubcategory.name}</div>
               </div>
-              <div className="ml-auto">{item.expenseSubcategory.name}</div>
-            </div>
-          </Flex>
-        }
+            </Flex>
+          }
+        />
+      </Card>
+      <CreateEditExpenseReportItemModal
+        open={isOpen}
+        setOpen={setIsOpen}
+        item={item}
       />
-    </Card>
+    </>
   );
 }
