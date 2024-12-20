6ff2a0a5640aa8976f63e2b611ca3fec01609a67
polina-fer <polina.rykova@fer.unizg.hr>
2024-12-20 22:38:49 +0100

    add export and action columns

--- frontend/src/features/employee/pages/TripRequests/components/TripsOverview.component.tsx
+++ frontend/src/features/employee/pages/TripRequests/components/TripsOverview.component.tsx
@@ -6,6 +6,8 @@ import useGetTripsByStatus from "@/hooks/useGetTripsByStatus";
 import { useState } from "react";
 import { GetEmployeeTripsByStatusStatusEnum } from "@/api_gen/apis/trip-controller-api";
 import { format } from "date-fns";
+import ActionButton from "../../Notifications/components/ActionButton.component";
+import { TripStatusStatusEnum } from "@/api_gen";
 
 export default function TripsOverview() {
   const [status, setStatus] = useState<
@@ -13,9 +15,10 @@ export default function TripsOverview() {
   >(undefined);
 
   const options: {
-    value: GetEmployeeTripsByStatusStatusEnum;
+    value: GetEmployeeTripsByStatusStatusEnum | null;
     label: string;
   }[] = [
+    { value: null, label: "All statuses" },
     {
       value: GetEmployeeTripsByStatusStatusEnum.PendingDepartmentApproval,
       label: "Pending Department Approval",
@@ -50,6 +53,7 @@ export default function TripsOverview() {
     },
     { value: GetEmployeeTripsByStatusStatusEnum.Paid, label: "Paid" },
   ];
+
   const columns = [
     {
       title: "Request number",
@@ -71,11 +75,34 @@ export default function TripsOverview() {
       dataIndex: "status",
       key: "status",
     },
+    {
+      title: "",
+      dataIndex: "action",
+      key: "action",
+    },
+    {
+      title: "",
+      dataIndex: "export",
+      key: "export",
+    },
   ];
+
+  const tripStatusFormat: Record<TripStatusStatusEnum, string> = {
+    PENDING_DEPARTMENT_APPROVAL: "Pending Department Approval",
+    DEPARTMENT_APPROVAL_REJECTED: "Department Approval Rejected",
+    TRAVEL_APPROVED: "Travel Approved",
+    PENDING_EXPENSE_APPROVAL: "Pending Expense Approval",
+    EXPENSE_APPROVAL_REJECTED: "Expense Approval Rejected",
+    PENDING_DIRECTOR_APPROVAL: "Pending Director Approval",
+    DIRECTOR_APPROVAL_REJECTED: "Director Approval Rejected",
+    AWAITING_PAYMENT: "Awaiting Payment",
+    PAID: "Paid",
+  };
 
   const { data } = useGetTripsByStatus(status);
 
   const extractedData = data?.content?.map((trip) => {
+    const status = tripStatusFormat[trip.status];
     return {
       requestNumber: trip.requestNumber,
       dateRange: `${format(new Date(trip.dateFrom), "dd.MM.yyyy")} - ${format(
@@ -83,7 +110,9 @@ export default function TripsOverview() {
         "dd.MM.yyyy"
       )}`,
       location: `${trip.address}, ${trip.city}, ${trip.country?.name || ""}`,
-      status: trip.status,
+      status: status,
+      action: <ActionButton status={trip.status} tripId={trip.id} />,
+      export: <Link>export</Link>,
     };
   });
 
@@ -105,7 +134,11 @@ export default function TripsOverview() {
           <Button>Create trip request</Button>
         </Link>
       </Flex>
-      <Table dataSource={extractedData} columns={columns}></Table>
+      <Table
+        dataSource={extractedData}
+        columns={columns}
+        locale={{ emptyText: "There are no trips with this status" }}
+      ></Table>
     </>
   );
 }
