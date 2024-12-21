6e25eca01b8778caaecc852a91ee1f28cd8a6790
polina-fer <polina.rykova@fer.unizg.hr>
2024-12-21 13:03:38 +0100

    add routing to department head requests

--- frontend/src/features/departmentHead/routes/departmentHead.routes.tsx
+++ frontend/src/features/departmentHead/routes/departmentHead.routes.tsx
@@ -2,11 +2,18 @@ import { createRoute } from "@tanstack/react-router";
 import { layoutRoute } from "../../../routes/router";
 import DepartmentStatistics from "../pages/DepartmentStatistics/DepartmentStatistics.page";
 import DepartmentApprovalRequests from "../pages/DepartmentApprovalRequests/DepartmentApprovalRequests.page";
+import DepartmentApprovalRequestReview from "../pages/DepartmentApprovalRequests/DepartmentApprovalRequestReview/DepartmentApprovalRequestReview.page";
 
 export const departmentApprovalRequestsRoute = createRoute({
   getParentRoute: () => layoutRoute,
   path: "/department-approval-requests",
   component: DepartmentApprovalRequests,
+});
+
+export const departmentApprovalRequestReviewRoute = createRoute({
+  getParentRoute: () => layoutRoute,
+  path: "/department-approval-requests/$id/review",
+  component: DepartmentApprovalRequestReview,
 });
 
 export const departmentStatisticsRoute = createRoute({
