c86fbf3d6ab8f09845e1be8dc883615bcf2af9a3
polina-fer <polina.rykova@fer.unizg.hr>
2024-12-26 21:19:39 +0100

    create awaiting payment table

---
+++ frontend/src/features/accountant/pages/Awaiting payment/AwaitingPayment.page.tsx
@@ -0,0 +1,72 @@
+import { Link } from "@tanstack/react-router";
+import { Button, Table } from "antd";
+import Title from "antd/es/typography/Title";
+import { format } from "date-fns";
+import useGetAwaitingPaymentTrips from "./hooks/useGetAwaitingPaymentTrips";
+
+export default function TripsOverview() {
+
+  const columns = [
+    {
+      title: "Request number",
+      dataIndex: "requestNumber",
+      key: "requestNumber",
+    },
+    {
+      title: "Employee",
+      dataIndex: "employee",
+      key: "employee",
+    },
+    {
+      title: "Date range",
+      dataIndex: "dateRange",
+      key: "dateRange",
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+      key: "location",
+    },
+    {
+      title: "Cost",
+      dataIndex: "cost",
+      key: "cost",
+    },
+    {
+      title: "",
+      dataIndex: "action",
+      key: "action",
+    },
+    {
+      title: "",
+      dataIndex: "export",
+      key: "export",
+    },
+  ];
+
+  const { data } = useGetAwaitingPaymentTrips();
+
+  const extractedData = data?.content?.map((trip) => {
+    return {
+      requestNumber: trip.requestNumber,
+      dateRange: `${format(new Date(trip.dateFrom), "dd.MM.yyyy")} - ${format(
+        new Date(trip.dateTo),
+        "dd.MM.yyyy"
+      )}`,
+      location: `${trip.address}, ${trip.city}, ${trip.country?.name || ""}`,
+      action: <Link><Button>Review Request</Button></Link>,
+      export: <Link>export</Link>,
+    };
+  });
+
+  return (
+    <>
+      <Title>Active Trip Requests</Title>
+      <Table
+        dataSource={extractedData}
+        columns={columns}
+        locale={{ emptyText: "There are no trips with this status" }}
+      ></Table>
+    </>
+  );
+}
--- frontend/src/features/accountant/pages/Awaiting payment/AwaitingPayment.tsx
+++
@@ -1,3 +0,0 @@
-export default function AwaitingPayment() {
-  return <div>Awaiting payment</div>;
-}
