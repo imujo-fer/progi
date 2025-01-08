d185f514bd5b5c272b366771bff6b499e457f014
adrianofaletar <adriano.faletar@fer.unizg.hr>
2025-01-08 20:21:03 +0100

    - sad

---
+++ frontend/src/features/admin/hooks/useGetDepartments.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { departmentApi } from "../../../apiClient"; // Adjust the path if necessary
+import { DepartmentDTO } from "@/api_gen";
+
+type UseGetDepartmentsProps = {
+  onSuccess?: (data: DepartmentDTO[]) => void;
+};
+
+export default function useGetDepartments({
+  onSuccess,
+}: UseGetDepartmentsProps) {
+  const query = useQuery({
+    queryKey: ["departments"],
+    queryFn: async () => {
+      const response = await departmentApi.getAllDepartments();
+      return response.data;
+    },
+  });
+
+  useEffect(() => {
+    if (!query.data) return;
+
+    onSuccess?.(query.data);
+  }, [query.data]);
+
+  return query;
+}
---
+++ frontend/src/features/admin/hooks/useInviteUser.tsx
@@ -0,0 +1,20 @@
+import { useMutation } from "@tanstack/react-query";
+import { UserInviteDTO } from "../../../api_gen";
+import { userApi } from "../../../apiClient";
+type UseInviteUserProps = {
+  onSuccess?: () => void;
+  onError?: (error: unknown) => void;
+};
+export default function useInviteUser({
+  onSuccess,
+  onError,
+}: UseInviteUserProps = {}) {
+  return useMutation({
+    mutationFn: async (userInvite: UserInviteDTO) => {
+      const response = await userApi.inviteUser({ userInviteDTO: userInvite });
+      return response.data;
+    },
+    onSuccess,
+    onError,
+  });
+}
