63638b6b81627b4bcc1201a886e0b80c1366c43a
imujo-fer <ivo.mujo.3@gmail.com>
2025-01-08 20:08:41 +0100

    fix dev mode for frontend

--- src/main/java/com/progi/congif/SpaController.java
+++ src/main/java/com/progi/congif/SpaController.java
@@ -1,12 +1,19 @@
 package com.progi.congif;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaController {
 
+    @Value("${env}")
+    private String env;
+
     @GetMapping(value = "/{path:^(?!api|swagger|oauth2|images|index.js|index.css).*$}/**")
-    public String forwardIndex() {
+    public String forwardIndex(Model model) {
+        model.addAttribute("env", env);
+
         return "index";
     }
 
